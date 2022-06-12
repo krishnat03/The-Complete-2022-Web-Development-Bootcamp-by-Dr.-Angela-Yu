9df74583c0ca209f5a70768da3811685b6b2fd51
Krishna Thakur <monumonu9820@gmail.com>
2022-06-13 02:42:40 +0530

    javascript started

---
+++ section-9-introduction to javascript/exercise2_quiz1.js
@@ -0,0 +1,16 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+var c = b;
+b = a;
+a = c;
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+test();
