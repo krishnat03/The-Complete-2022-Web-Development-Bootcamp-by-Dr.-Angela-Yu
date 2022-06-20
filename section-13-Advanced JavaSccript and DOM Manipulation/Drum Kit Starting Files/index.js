34d5034e73e3291616a6b944890a116e5c7afb39
Krishna Thakur <monumonu9820@gmail.com>
2022-06-21 00:55:05 +0530

    starting section

---
+++ section-13-Advanced JavaSccript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,5 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){alert("I got clicked");})
+}
