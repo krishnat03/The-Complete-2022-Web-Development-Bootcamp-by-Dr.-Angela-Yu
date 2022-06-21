58f61c3d66163091f0663586208811592a8b0bab
Krishna Thakur <monumonu9820@gmail.com>
2022-06-22 01:37:47 +0530

    jQuery Starter

---
+++ section-14-jQuery/selecting element with jQuery/index.js
@@ -0,0 +1,9 @@
+// in normal JS
+// document.querySelector("h1");
+
+// using jQuery
+    $("h1");   //selecting one.
+
+    // document.querySelectorAll("button");
+    $("button");  //selecting many
+
