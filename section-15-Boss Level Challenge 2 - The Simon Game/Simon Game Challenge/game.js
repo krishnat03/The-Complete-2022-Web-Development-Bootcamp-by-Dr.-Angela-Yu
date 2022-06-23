1dd8686e50ad2c8164d8d06d53f7f6a6989be4e3
Krishna Thakur <monumonu9820@gmail.com>
2022-06-23 20:30:35 +0530

    section-15 completed

--- section-15-Boss Level Challenge 2 - The Simon Game/Simon Game Challenge/game.js
+++ section-15-Boss Level Challenge 2 - The Simon Game/Simon Game Challenge/game.js
@@ -2,25 +2,96 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
 
-$(".btn").click(function(){
-    var userChosenColour = $(this).attr("id");
-    // var userChosenColour = document.querySelector(".btn").getAttribute("id");
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+$(document).keydown(function () {
+    if (!started) {
+        $("#level-title").text("level" + level);
+        nextSequence();
+        started = true;
+    }
 });
 
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    // var userChosenColour = document.querySelectorAll(".btn").getAttribute("id");  // error
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
 
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 
-function nextSequence(){
-    var randomNumber =  Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColour);
-    $("#" + randomChosenColour).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
-    
-    var audio = new Audio('sounds/' + randomChosenColour + ".mp3");
-    audio.play();
+    checkAnswer(userClickedPattern.length - 1);
+});
 
+// Aleternate
+// const buttons = document.querySelectorAll(".btn")
+// buttons.forEach((e)=>{
+//     e.addEventListener("click", ()=>{
+//         userClickedPattern.push(e.getAttribute("id"));
+//         console.log(userClickedPattern);
+//     })
+// })
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        // console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        // console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
 }
 
-nextSequence();
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour)
+        .fadeOut(50)
+        .fadeIn(50)
+        .fadeOut(50)
+        .fadeIn(50);
+
+    // var audio = new Audio('sounds/' + randomChosenColour + ".mp3");
+    // audio.play();
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// nextSequence();
