b61124b63f26bc5019c7efbb3ae0be9fec98acf6
Krishna Thakur <monumonu9820@gmail.com>
2022-06-28 15:50:29 +0530

    finished Weather project

--- section-20-API's-Application Programming Interfaces/WeatherProject/app.js
+++ section-20-API's-Application Programming Interfaces/WeatherProject/app.js
@@ -1,26 +1,60 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
 
 
 app.get('/', (req, res) => {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Thane&appid=e31608f1dc6ef2a34e0c14e395b576a5&units=metric"
-    https.get(url, (response) => {
-        console.log(response.statusCode);
-    });
+    res.sendFile(__dirname + "/index.html");
+    
 
 
 
-    res.send("server is up and running");
+    // res.send("server is up and running");
 
 });
 
+app.post("/", (req, res) => {
+    // console.log("Post Recieved");
+    // console.log(req.body.cityName);
 
+    const query = req.body.cityName;
+    const apiKey = "e31608f1dc6ef2a34e0c14e395b576a5";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit + "";
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+
+        response.on('data', (data) => {
+            const weatherData = JSON.parse(data);
+            // console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            // console.log(temp);
+            // console.log(weatherDescription);
+            
+            res.write("<h1>The weather in " + query + " is " + temp + " degrees Celcius.</h1>");
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+
+            // const object = {
+            //     name: "Krishna",
+            //     Age: 22
+            // }
+            // console.log(JSON.stringify(object));
+
+        });
+    });
+})
 
 
 
