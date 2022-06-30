3659545d8a7a051e314d2c96f1bf3e28f8181751
Krishna Thakur <monumonu9820@gmail.com>
2022-06-30 15:52:50 +0530

    second commit

--- section-22-EJS/todolist-v1/app.js
+++ section-22-EJS/todolist-v1/app.js
@@ -10,15 +10,42 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     
     var today = new Date();
+    // console.log(today);
     var currentDay = today.getDay();
+    // console.log(currentDay);
     var day = "";
-    if(currentDay === 6 || currentDay === 0){
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+    
+        default:
+            console.log("Error: current is equal to: " + currentDay)
+            break;
     }
 
-    res.render("list", {kindOfDay : day});
+    res.render("list", {
+        kindOfDay : day
+    });
 });
 
 app.listen(port, () => {
