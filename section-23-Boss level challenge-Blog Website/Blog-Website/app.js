e7e95908824596502b6dcdfbf5683321bee4111a
Krishna Thakur <monumonu9820@gmail.com>
2022-07-04 02:00:57 +0530

    section-23 completed

--- section-23-Boss level challenge-Blog Website/Blog-Website/app.js
+++ section-23-Boss level challenge-Blog Website/Blog-Website/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
+const _ = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,56 +18,51 @@ let posts = [];
 
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public")); 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 app.get('/', (req, res) => {
   res.render("home", {
-    paragraph1: homeStartingContent,  
+    paragraph1: homeStartingContent,
     posts: posts
   });      //{key: value}
-  
 });
 
 app.get('/about', (req, res) => {
-  res.render("about", {aboutContent: aboutContent});      //{key: value}
+  res.render("about", { aboutContent: aboutContent });      //{key: value}
 });
 
 app.get('/contact', (req, res) => {
-  res.render("contact", {contactContent: contactContent});      //{key: value}
+  res.render("contact", { contactContent: contactContent });      //{key: value}
 });
 
 app.get('/compose', (req, res) => {
   res.render("compose");
 });
 
-
 //route paramaters
 app.get('/posts/:postName', (req, res) => {
-  let requestedTitle= req.params.postName;
-  requestedTitle = requestedTitle.toLowerCase();
+  const requestedTitle = _.lowerCase(req.params.postName);
+
   posts.forEach((post) => {
-    const storedTitle = post.title;
-    if(storedTitle === requestedTitle){
-      console.log("Match found");
-    } else{
-      console.log("Not a Match");
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      res.render("post", { post_title: post.title, post_content: post.content });
     }
   });
 })
 
+
 app.post('/compose', (req, res) => {
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody 
+    content: req.body.postBody
   }
   posts.push(post);
   res.redirect('/');
 
 });
-
 
 
 app.listen(port, () => {
