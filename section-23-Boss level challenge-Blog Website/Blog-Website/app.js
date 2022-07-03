565beb314aa8a67f0fc5df13b398e043dba5f208
Krishna Thakur <monumonu9820@gmail.com>
2022-07-03 23:53:41 +0530

    blog midway

--- section-23-Boss level challenge-Blog Website/Blog-Website/app.js
+++ section-23-Boss level challenge-Blog Website/Blog-Website/app.js
@@ -1,12 +1,73 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require("ejs");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+const port = 3000;
+
+
+let posts = [];
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); 
+
+
+
+app.get('/', (req, res) => {
+  res.render("home", {
+    paragraph1: homeStartingContent,  
+    posts: posts
+  });      //{key: value}
+  
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.get('/about', (req, res) => {
+  res.render("about", {aboutContent: aboutContent});      //{key: value}
+});
+
+app.get('/contact', (req, res) => {
+  res.render("contact", {contactContent: contactContent});      //{key: value}
+});
+
+app.get('/compose', (req, res) => {
+  res.render("compose");
+});
+
+
+//route paramaters
+app.get('/posts/:postName', (req, res) => {
+  let requestedTitle= req.params.postName;
+  requestedTitle = requestedTitle.toLowerCase();
+  posts.forEach((post) => {
+    const storedTitle = post.title;
+    if(storedTitle === requestedTitle){
+      console.log("Match found");
+    } else{
+      console.log("Not a Match");
+    }
+  });
+})
+
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody 
+  }
+  posts.push(post);
+  res.redirect('/');
+
+});
+
+
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
 });
