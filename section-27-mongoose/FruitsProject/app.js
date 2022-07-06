a0a4f55d4185e9f105c547f4258291f213f67e55
Krishna Thakur <monumonu9820@gmail.com>
2022-07-06 20:12:41 +0530

    sections-27 completed

--- section-27-mongoose/FruitsProject/app.js
+++ section-27-mongoose/FruitsProject/app.js
@@ -8,29 +8,67 @@ const fruitSchema = new mongoose.Schema ({
     review: String
 });
 
+//Data Validation
+// const fruitSchema = new mongoose.Schema ({
+//     name: {
+//         type: String,
+//         required: [true, "Please Check your data entry, no name specified!"]
+//     },
+//     rating: {
+//         type: Number,
+//         min: 1,
+//         max: 10
+//     },
+//     review: String
+// });
+
 const Fruit = mongoose.model("Fruit", fruitSchema);    // to make collection
 
 const fruit = new Fruit ({
-    name: "Apple",
-    rating: 8,
-    review: "Great fruit" 
+    name: "Peach",
+    rating: 10,
+    review: "Dragon Fruit is the best" 
 });
+
+// const fruit = new Fruit ({
+                                           // it will show error since the name is validated or checked
+//     rating: 10,
+//     review: "Great fruit" 
+// });
 
 // fruit.save();
 
 const personSchema = new mongoose.Schema ({
     name: String,
-    age: Number
+    age: Number,
+    favouriteFruit: fruitSchema
 });
 
 const Person = mongoose.model("Person", personSchema);
 
+
+// relationship
+const pineapple = new Fruit ({
+    name: "Pineapple",
+    rating: 10,
+    review: "Fruit is the best" 
+});
+
+// pineapple.save();
+
+// const person = new Person ({
+//     name: "John",
+//     age: 37 
+// });
+
 const person = new Person ({
-    name: "John",
-    age: 37 
+    name: "Soham",
+    age: 12,
+    favouriteFruit: pineapple
 });
 
 // person.save();
+//relationship
 
 // const kiwi = new Fruit ({
 //     name: "Kiwi",
@@ -72,3 +110,52 @@ Fruit.find((err, fruits) => {
         });
     }
 });
+
+//updation
+
+// Fruit.updateOne({_id: "62c58e4cf9ba4c8ca7edd745"}, {name: "Dragon Fruit"}, (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Sucessfully updated the document")
+//     }
+// });
+
+
+//deletion
+// Fruit.deleteOne({_id: "62c58e4cf9ba4c8ca7edd745"},(err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Sucessfully deleted the document")
+//         }
+// });
+
+// Person.deleteMany({name:"John"}, (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Sucessfully deleted the document")
+//         }
+// });
+
+
+//relationship
+
+
+
+const mango = new Fruit ({
+    name: "Mango",
+    rating: 9,
+    review: "Mango is the best" 
+});
+
+mango.save();
+
+Person.updateOne({name:"John"}, {favouriteFruit: mango}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Sucessfully updated the document")
+    }
+})
