e2fe8b75effd85531488ecec77d1ee6f48d47366
Krishna Thakur <monumonu9820@gmail.com>
2022-07-16 16:33:44 +0530

    Register Users with Username and password

---
+++ section-32-Authentication & Security/Secrets/app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+
+const User = new mongoose.model('User', userSchema);
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if(foundUser.password === password){
+                    res.render('secrets');
+                } else {
+                    res.send("Oops! Please type the correct username and Password");
+                }
+            }
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
