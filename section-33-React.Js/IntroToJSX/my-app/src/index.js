5945184aaaf5ad49caaef9357a84020633a52326
Krishna Thakur <monumonu9820@gmail.com>
2022-07-23 18:36:35 +0530

    JSX code Practice

--- section-33-React.Js/IntroToJSX/my-app/src/index.js
+++ section-33-React.Js/IntroToJSX/my-app/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is paragraph</p>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
