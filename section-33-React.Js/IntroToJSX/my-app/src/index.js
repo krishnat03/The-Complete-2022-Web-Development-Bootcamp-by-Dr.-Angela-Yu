2908815e0bc53c92819600c57c0f4270c66d56b3
Krishna Thakur <monumonu9820@gmail.com>
2022-07-23 19:21:45 +0530

    JavaScript Expressions in JSX and ES6 templates

--- section-33-React.Js/IntroToJSX/my-app/src/index.js
+++ section-33-React.Js/IntroToJSX/my-app/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "Krishna";
+const lname = "Thakur";
+const luckyNumber = "7";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1>Hello {`${fname} ${lname}`}!</h1>
+    <p>Your Lucky number is {luckyNumber}</p>
   </div>,
-  document.getElementById("root")
-);
+document.getElementById("root"));
