c2718bb283fa42e2ba6e5458b32a3b1bbac31279
Krishna Thakur <monumonu9820@gmail.com>
2022-07-24 01:39:10 +0530

    React styling practice

--- section-33-React.Js/IntroToJSX/my-app/src/index.js
+++ section-33-React.Js/IntroToJSX/my-app/src/index.js
@@ -1,17 +1,28 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
+import { React } from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
+const date = new Date();
+
+const currentTime = date.getHours();
+
+let greeting;
+
 const customStyle = {
-  color: "red",
-  fontStyle: "2rem",
-  border: "1px solid black",
+  color : "",
 }
 
-customStyle.color = "blue";
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
 
-root.render(
-  <h1 style={customStyle}>Hello World!</h1>
-);
+root.render(<h1 className="heading" style={customStyle}>{greeting}</h1>);
