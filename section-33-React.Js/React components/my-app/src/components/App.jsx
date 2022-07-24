6bb27673a8a417f7b5370942c0f7b1721bbad679
Krishna Thakur <monumonu9820@gmail.com>
2022-07-24 17:31:41 +0530

    React components

---
+++ section-33-React.Js/React components/my-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+const App = () => {
+    return <div>
+            <Heading />
+            <List />
+        </div>
+}
+
+export default App;
---
+++ section-33-React.Js/React components/my-app/src/components/List.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+const List = () => {
+  return (
+      <ul>
+        <li>Banana</li>
+        <li>JackFruit</li>
+        <li>Melon</li>
+      </ul>
+  );
+};
+
+export default List;
