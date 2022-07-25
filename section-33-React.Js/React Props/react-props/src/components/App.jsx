c86b148460d4976269c7feb609a7a4e005925ac5
Krishna Thakur <monumonu9820@gmail.com>
2022-07-25 23:22:41 +0530

    React props

---
+++ section-33-React.Js/React Props/react-props/src/components/App.jsx
@@ -0,0 +1,19 @@
+import { React } from "react";
+import Header from "./Header"
+import Note from "./Note";
+import Footer from "./Footer"
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
+
+
