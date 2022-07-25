52927f4c4cef42633b02e7ca4e229e2ce639c706
Krishna Thakur <monumonu9820@gmail.com>
2022-07-25 15:19:08 +0530

    import export and modules practice

---
+++ section-33-React.Js/import Export and modules/my-app/src/calculator.js
@@ -0,0 +1,14 @@
+const add = (n1, n2) => {
+  return n1 + n2;
+}
+const subtract = (n1, n2) => {
+  return n1 - n2;
+}
+const multiply = (n1, n2) => {
+  return n1 * n2;
+}
+const divide = (n1, n2) => {
+  return n1 / n2;
+}
+
+export {add, subtract, multiply, divide};
--- section-33-React.Js/import Export and modules/my-app/src/index.js
+++ section-33-React.Js/import Export and modules/my-app/src/index.js
@@ -1,8 +1,9 @@
 import { React } from "react";
 import { createRoot } from "react-dom/client";
 // import pi, {doublePi, triplePi} from "./math";
-import * as pi from "./math";
+// import * as pi from "./math";
 
+import * as Calculator from "./calculator"
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -11,8 +12,9 @@ const root = createRoot(container);
 
 root.render(
   <ul>
-    <li>{pi.default}</li>
-    <li>{pi.doublePi()}</li>
-    <li>{pi.triplePi()}</li>
+    <li>{Calculator.add(1, 2)}</li>
+    <li>{Calculator.subtract(2, 3)}</li>
+    <li>{Calculator.multiply(7, 2)}</li>
+    <li>{Calculator.divide(5, 2)}</li>
   </ul>
 );
