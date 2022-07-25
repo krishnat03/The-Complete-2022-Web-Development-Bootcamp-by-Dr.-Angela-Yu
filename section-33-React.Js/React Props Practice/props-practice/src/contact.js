b3122b9cf476238dd88aa8df9c17f619e1310187
Krishna Thakur <monumonu9820@gmail.com>
2022-07-26 00:18:01 +0530

    React Props Practice

---
+++ section-33-React.Js/React Props Practice/props-practice/src/contact.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+        name: "Sachin Tendulkar",
+        imgURL:
+            "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316400/316486.png",
+        phone: "+123 456 789",
+        email: "sachin@tendulkar.com"
+    },
+    {
+        name: "Mahendra Singh Dhoni",
+        imgURL:
+            "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/319900/319946.png",
+        phone: "+987 654 321",
+        email: "dhoni@dhoni.com"
+    },
+    {
+        name: "Virat Kholi",
+        imgURL:
+            "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316600/316605.png",
+        phone: "+918 372 574",
+        email: "virat@kholi.com"
+    }
+];
+
+export default contacts;
