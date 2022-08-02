e024730a45dd25122147b43e99dbc3cd320f4b09
Krishna Thakur <monumonu9820@gmail.com>
2022-08-02 17:30:45 +0530

    Destructuring challenge

--- section-33-React.Js/ES6 object and array Destructuring/destructuring-arrays-objects/src/index.js
+++ section-33-React.Js/ES6 object and array Destructuring/destructuring-arrays-objects/src/index.js
@@ -1,47 +1,57 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import animals, {useAnimals} from "./data";
+// import animals, {useAnimals} from "./data";
+import cars from "./practice";
 
-//destructuring arrays.
-// console.log(animals);
-const [cat, dog] = animals;
-// console.log(dog);
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-Mechanimals();
-function Mechanimals() {
-    const [animal, makeSound] = useAnimals(cat);
-    console.log(animal);
-    makeSound();
-}
+const [honda, tesla] = cars;
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla
+const {coloursByPopularity: [hondaTopColour]} = honda
+const {coloursByPopularity: [teslaTopColour]} = tesla
+
+root.render(
+    <table>
+        <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+            <th>Top Colour</th>
+        </tr>
+        <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+        </tr>
+    </table>
+);
 
 
 
-//destructuring objects.
-// const {name, sound} = cat;
-// const {name: catName , sound: catSound} = cat;
-// const {name = "fluffy", sound = "Purr"} = cat;
-// const {name, sound, feedingRequirements: {food, water}} = cat;
-// const {feedingRequirements: {food, water}} = cat;
-// console.log(food);
 
-// const container = document.getElementById("root");
-// const root = createRoot(container);
+// //destructuring arrays.
+// // console.log(animals);
+// const [cat, dog] = animals;
+// // console.log(dog);
 
-// root.render(
-//     <table>
-//         <tr>
-//             <th>Top Speed</th>
-//             <th>Brand</th>
-//         </tr>
-//         <tr>
-//             <td>{tesla.model}</td>
-//             <td>{teslaTopSpeed}</td>
-//             <td>{teslaTopColour}</td>
-//         </tr>
-//         <tr>
-//             <td>{honda.model}</td>
-//             <td>{hondaTopSpeed}</td>
-//             <td>{hondaTopColour}</td>
-//         </tr>
-//     </table>
-// );
+// Mechanimals();
+// function Mechanimals() {
+//     const [animal, makeSound] = useAnimals(cat);
+//     console.log(animal);
+//     makeSound();
+// }
+
+
+// //destructuring objects.
+// // const {name, sound} = cat;
+// // const {name: catName , sound: catSound} = cat;
+// // const {name = "fluffy", sound = "Purr"} = cat;
+// // const {name, sound, feedingRequirements: {food, water}} = cat;
+// // const {feedingRequirements: {food, water}} = cat;
+// // console.log(food);
