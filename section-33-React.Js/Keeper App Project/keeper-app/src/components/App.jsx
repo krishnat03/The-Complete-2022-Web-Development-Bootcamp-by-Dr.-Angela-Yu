959b2125cfcec9fc2119a94530233f9832ee9575
Krishna Thakur <monumonu9820@gmail.com>
2022-08-07 01:47:36 +0530

    kepper App project part-3

--- section-33-React.Js/Keeper App Project/keeper-app/src/components/App.jsx
+++ section-33-React.Js/Keeper App Project/keeper-app/src/components/App.jsx
@@ -1,24 +1,46 @@
-import { React } from "react";
+import React, { useState } from "react";
 import Header from "./Header"
 import Note from "./Note";
 import Footer from "./Footer"
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 const App = () => {
+
+    const [notes, setNotes] = useState([]);
+
+    const addNote = (newNote) => {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        })
+    }
+
+    const deleteNote = (id) => {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(noteItem => <Note
-                key={noteItem.key}
-                title={noteItem.title}
-                content={noteItem.content}
+            <CreateArea
+                onAdd={addNote}
             />
-            )}
+            {notes.map((noteItem, index) => {
+                return <Note
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            })}
             <Footer />
         </div>
     )
 }
 
 export default App;
-
-
