a28a1686cb3df1b153193963da231a404ae0149e
Krishna Thakur <monumonu9820@gmail.com>
2022-08-07 02:52:46 +0530

    styling keeper app using Material UI

--- section-33-React.Js/Keeper App Project/keeper-app/src/components/CreateArea.jsx
+++ section-33-React.Js/Keeper App Project/keeper-app/src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
 
   const [note, setNote] = useState({
     title: "",
@@ -27,21 +32,33 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  const expand = () => {
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
+      <form className="create-note">
+        {isExpanded && (<input
           name="title"
           onChange={handleChange}
           value={note.title}
-          placeholder="Title" />
+          placeholder="Title"
+        />
+        )}
         <textarea
           name="content"
+          onClick={expand}
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3" />
-        <button onClick={submitNote}>Add</button>
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- section-33-React.Js/Keeper App Project/keeper-app/src/components/Header.jsx
+++ section-33-React.Js/Keeper App Project/keeper-app/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 const Header = () => {
   return (
     <header>
-        <h1>Keeper</h1>
+      <h1><HighlightIcon />Keeper</h1>
     </header>
-    
+
   )
 }
 
